31a420f607414f05adb60db187cf2d011d1e1317
Mostefaouim <mohammedmostefaoui2@gmail.com>
2024-12-03 21:14:50 +0100

    Initial commit

--- index.php
+++ index.php
@@ -14,32 +14,44 @@ if (!isset($_SESSION['user']) || $_SESSION['role'] !== 'admin') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Navigation Menu</title>
     <style>
-        a{
+        a {
             text-decoration: none;
             color: black;
-            margin-top: 20px;
+            margin: 10px;
+            display: inline-block;
+            padding: 10px 15px;
+            background-color: #f0f0f0;
+            border: 1px solid #ccc;
+            border-radius: 5px;
         }
-        form{
+
+        a:hover {
+            background-color: #e0e0e0;
+        }
+
+        .menu {
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
             margin: 25px;
-            margin-left: 12px;
         }
     </style>
 </head>
 
 <body>
-    <form method="post">
-        <button type="submit" formaction="etudiant/index.php" title="Manage Students">Etudiants</button>
-        <button type="submit" formaction="Enseignants/" title="Manage Teachers">Enseignants</button>
-        <button type="submit" formaction="Modules/index.php" title="Manage Modules">Modules</button>
-        <button type="submit" formaction="Bulletin/index.php" title="View Bulletins">Bulletin de Note</button>
-        <button type="submit" formaction="Bulletin/pv.php" title="View PV">PV</button>
-        <button type="submit" formaction="Static/statistiques.php" title="View Statistics">Statistiques</button>
-        <button type="submit" formaction="PV/pv.php" title="View PV Statistics">PV Statistiques</button>
-        <button type="submit" formaction="users/display.php" title="User List">Liste D'utilisateur</button>
-        <button type="submit" formaction="users/graph.php" title="User Statistics">Users Statistiques</button>
-        <button type="submit" formaction="table.php" title="View Tables">Tables</button>
-    </form><br>
-    <center><a href="users/logout.php">Deconnecter</a></center>
+    <div class="menu">
+        <a href="etudiant/index.php" title="Manage Students">Etudiants</a>
+        <a href="Enseignants/" title="Manage Teachers">Enseignants</a>
+        <a href="Modules/index.php" title="Manage Modules">Modules</a>
+        <a href="Bulletin/index.php" title="View Bulletins">Bulletin de Note</a>
+        <a href="Bulletin/pv.php" title="View PV">PV</a>
+        <a href="Static/statistiques.php" title="View Statistics">Statistiques</a>
+        <a href="PV/pv.php" title="View PV Statistics">PV Statistiques</a>
+        <a href="users/display.php" title="User List">Liste D'utilisateur</a>
+        <a href="users/graph.php" title="User Statistics">Users Statistiques</a>
+        <a href="table.php" title="View Tables">Tables</a>
+    </div>
+    <center><a href="users/logout.php" style="margin-top: 20px;">Deconnecter</a></center>
 </body>
 
 </html>
